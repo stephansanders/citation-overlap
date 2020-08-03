ba3d0957b356bdddce103b57355f25e5fa1a2dcf
David Young <david@textflex.com>
2020-08-03 18:50:12 +0800

    Initial web app using Flask
    Add a Web Server Gateway Interface (WSGI) web app using the Flask framework. This web app accepts the raw database files as CSV-formatted strings converted to JSON. All databases are processed together, and the extracted results and overlaps data frame are returned as JSON output.

---
+++ wsgi.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import io
+import logging
+
+from flask import Flask, request, jsonify
+import pandas as pd
+
+import medline_embase_scopus
+
+logging.basicConfig(filename='out.log',level=logging.INFO)
+app = Flask(__name__)
+
+
+def findOverlaps(data):
+	dbExtractor = medline_embase_scopus.DbExtractor()
+	dfs = {}
+	for key, val in data.items():
+		path = f'{key}.csv'
+		df = pd.read_csv(
+			io.StringIO(val), sep=',', index_col=False, dtype=str,
+			na_filter=False)
+		df.to_csv(f'{key}_yeeeah.csv', index=False)
+		extractorPath = os.path.join(
+			'flask', medline_embase_scopus.PATH_EXTRACTORS, f'{key}.yml')
+		app.logger.info(path) #, extractorPath, val)
+		app.logger.info(extractorPath)
+		df, dbName = dbExtractor.extractDb(f'{key}_in.csv', extractorPath, df)
+		dfs[key] = df
+	dfs['overlaps'] = dbExtractor.combineOverlaps()
+	return dfs
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+	if request.method == 'POST':
+		jsonData = request.get_json()
+		dfs = findOverlaps(jsonData)
+		dfs = {k: v.to_json(orient='records') for k, v in dfs.items()}
+		return jsonify(dfs)
+	return 'Hello my friend!'
+
+
+if __name__ == "__main__":
+	main()
+
