f68a97a11d7bee0a4faf3576079b8a6b64785ae7
David Young <david@textflex.com>
2021-03-15 19:14:29 +0800

    Basic run script
    Add a basic Python launch script as an entry point to the application.

---
+++ run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Simple startup script for Citation-Overlap
+
+from citov import gui
+
+
+def main():
+	"""Start the graphical interface."""
+	gui.main()
+
+
+if __name__ == "__main__":
+	print("Starting Citation-Overlap run script...")
+	main()
