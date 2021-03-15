9c8c0e9899d131bd983425088417e7faf62338ff
David Young <david@textflex.com>
2021-03-15 11:22:08 +0800

    Initial Python setup script
    Provide a basic setup script with required dependencies specified.

---
+++ setup.py
@@ -0,0 +1,28 @@
+# Citation-Overlap setup script
+
+import setuptools
+
+# installation configuration
+config = {
+	"name": "citation-overlap",
+	"description": "Converge citations from scientific journal databases",
+	"author": "Stephan Sanders Lab",
+	"url": "https://github.com/stephansanders/citation-overlap",
+	"author_email": "",
+	"version": "1.0.0",
+	"packages": setuptools.find_packages(),
+	"scripts": [],
+	"python_requires": ">=3.6",
+	"install_requires": [
+		"PyQt5",
+		"traitsui",
+		"jellyfish",
+		"pandas",
+		"pyyaml",
+	],
+}
+
+
+if __name__ == "__main__":
+	# perform setup
+	setuptools.setup(**config)
