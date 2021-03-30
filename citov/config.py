c5006c3a22df853064092950908b3a88e2ad17af
David Young <david@textflex.com>
2021-03-30 16:26:36 +0800

    Initial configuration module
    Add a basic configuration module to store app settings.

---
+++ citov/config.py
@@ -0,0 +1,10 @@
+# Citation-Overlap configuration
+"""Configuration settings for Citation-Overlap."""
+
+import pathlib
+
+#: str: Application name.
+APP_NAME = "Citation-Overlap"
+
+#: :class:`pathlib.Path`: Application directory.
+app_dir = pathlib.Path(__file__).resolve().parent.parent
