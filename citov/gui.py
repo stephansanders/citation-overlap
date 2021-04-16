stephansanders/citation-overlap

commit 4027502d5d0d3cebbd9169fbdb8e6231fc609866

Fix finding extractor paths outside of the app root folder
Use the app's directory to construct the path to the extractor folder rather than using a relative path assumed to be in the app root folder. Limit the names of extractors in the GUI to the filenames rather than the full path since this path may be long and widen the window.

citov/gui.py
@@ -150,7 +150,8 @@ class CiteOverlapGUI(HasTraits):
 			Item(
 				"_extractor", label="Extractor",
 				editor=CheckListEditor(
-					name="object._extractorNames.selections")),
+					name="object._extractorNames.selections",
+					format_func=lambda x: x)),
 		),
 		Item('_overlapBtn', show_label=False),
 		HGroup(
@@ -191,8 +192,8 @@ class CiteOverlapGUI(HasTraits):
 		# populate drop-down of available extractors
 		self._extractorNames = TraitsList()
 		extractorNames = [self._DEFAULT_EXTRACTOR]
-		extractorNames.extend(glob.glob(
-			os.path.join(medline_embase_scopus.PATH_EXTRACTORS, "*")))
+		extractorNames.extend([os.path.basename(f) for f in glob.glob(
+			str(medline_embase_scopus.PATH_EXTRACTORS / "*"))])
 		self._extractorNames.selections = extractorNames
 		self._extractor = self._extractorNames.selections[0]
 
@@ -285,9 +286,8 @@ class CiteOverlapGUI(HasTraits):
 			# auto-select extractor based on given database
 			if extractor:
 				# use given extractor
-				extractorPath = os.path.join(
-					medline_embase_scopus.PATH_EXTRACTORS,
-					extractor.value)
+				extractorPath = medline_embase_scopus.PATH_EXTRACTORS / \
+					extractor.value
 			else:
 				# defer finding extractor to the extractor function
 				extractorPath = None

citov/medline_embase_scopus.py
@@ -17,6 +17,8 @@ import jellyfish # string comparison # pip3 install jellyfish
 import pandas as pd
 import yaml
 
+from citov import config
+
 # How to perform the search
 
 # Pubmed
@@ -44,14 +46,14 @@ import yaml
 # Click arrow next to 'All' above the column headings and below
 # 'Analyze search results'; click 'Select all'
 # Click 'Export'
-# Select 'CSV Export' 
+# Select 'CSV Export'
 # Add PubMed ID (you may want to add Abstract too)
 # Click Export
 # Combine the lists in a text editor or Google Sheet, not in Excel
 
 
 #: str: Path to extractor specification folder.
-PATH_EXTRACTORS = '../extractors'
+PATH_EXTRACTORS = config.app_dir / 'extractors'
 
 
 class DefaultExtractors(Enum):
@@ -208,8 +210,8 @@ class DbExtractor:
 			# identify a YAML extractor for the given database based on first
 			# part of the path filename
 			pathDbSplit = os.path.basename(os.path.splitext(path)[0]).split('_')
-			extractorPaths = glob.glob(os.path.join(
-				PATH_EXTRACTORS, f'{pathDbSplit[0].lower()}.*'))
+			extractorPaths = glob.glob(
+				str(PATH_EXTRACTORS / f'{pathDbSplit[0].lower()}.*'))
 			for extrPath in extractorPaths:
 				if os.path.splitext(extrPath.lower())[1] in ('.yml', '.yaml'):
 					# case-insensitive match for YAML extension

citov/wsgi.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import os
 import io
 import logging
+import pathlib
 
 from flask import Flask, request, jsonify
 import pandas as pd
@@ -21,7 +21,7 @@ def findOverlaps(data):
 		df = pd.read_csv(
 			io.StringIO(val), sep=',', index_col=False, dtype=str,
 			na_filter=False)
-		extractorPath = os.path.join(
+		extractorPath = pathlib.Path(
 			'citation-overlap', medline_embase_scopus.PATH_EXTRACTORS,
 			f'{key}.yml')
 		app.logger.info(path)

commit da9244e35d359c2975af717bfd25cf4a5d00eee1

Fix finding path to user-specified extractors in GUI
Commit 4027502d5d0d3cebbd9169fbdb8e6231fc609866 introduced a regression where the full path was lost from extractors to display only the basename in the GUI dropdown box. Map these extractor names to their full paths to load user-selected extractors.

citov/gui.py
@@ -190,11 +190,15 @@ class CiteOverlapGUI(HasTraits):
 		"""Initialize the GUI."""
 		super().__init__()
 
-		# populate drop-down of available extractors
+		# populate drop-down of available extractors from directory of
+		# extractors, displaying only basename but keeping dict with full path
 		self._extractorNames = TraitsList()
 		extractorNames = [self._DEFAULT_EXTRACTOR]
-		extractorNames.extend([os.path.basename(f) for f in glob.glob(
-			str(medline_embase_scopus.PATH_EXTRACTORS / "*"))])
+		extractor_paths = glob.glob(
+			str(medline_embase_scopus.PATH_EXTRACTORS / "*"))
+		self._extractor_paths = {
+			os.path.basename(f): f for f in extractor_paths}
+		extractorNames.extend(self._extractor_paths.keys())
 		self._extractorNames.selections = extractorNames
 		self._extractor = self._extractorNames.selections[0]
 
@@ -292,6 +296,9 @@ class CiteOverlapGUI(HasTraits):
 			else:
 				# defer finding extractor to the extractor function
 				extractorPath = None
+		else:
+			# get full path for selected extractor
+			extractorPath = self._extractor_paths[extractorPath]
 
 		# extract file
 		df, dbName = self.dbExtractor.extractDb(path, extractorPath)