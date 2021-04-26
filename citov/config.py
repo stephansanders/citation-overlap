726dee32bde494febc4674296fe96a6334286642
David Young <david@textflex.com>
2021-04-26 18:14:55 +0800

    Make `appdirs` a dependency to find standard user application directories
    Add the `appdirs` library as a dependency to find standard directories for applications to store application data.

--- citov/config.py
+++ citov/config.py
@@ -3,8 +3,12 @@
 
 import pathlib
 
+from appdirs import AppDirs
+
 #: str: Application name.
 APP_NAME = "Citation-Overlap"
 
 #: :class:`pathlib.Path`: Application directory.
 app_dir = pathlib.Path(__file__).resolve().parent.parent
+#: str: Accessor to application-related user directories.
+user_app_dirs = AppDirs(APP_NAME, False)
